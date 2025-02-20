ed804c7b51d9868299b64f7cca83d914fee539b7
20-100T <vinct27@hotmail.ca>
2025-02-20 12:42:08 -0500

    Process MainDecoder fait 99% a verifier

--- src/controller.vhd
+++ src/controller.vhd
@@ -8,94 +8,100 @@ USE IEEE.STD_LOGIC_1164.all;
 ENTITY controller IS 
 PORT (
 	OP, Funct: IN std_logic_vector(5 downto 0);
-	MemtoReg, MemWrite, MemRead, Branch, AluSrc,
-	RegDst, RegWrite, MemRead, Branch, AluSrc,
-	AluControl : OUT std_logi_vector(3 downto 0));
+	MemtoReg, MemWrite, MemRead, Branch, ALUSrc,
+	RegDst, RegWrite, Jump: OUT std_logic; 
+	AluControl: OUT std_logic_vector(3 downto 0));
 
 END Controller;
 
 ARCHITECTURE logique of controller is 
 
+  SIGNAL MtoReg, MemW, MemR, MRead, Bran, ALUS, RegD, RegW, J : STD_LOGIC;
+  SIGNAL ALUCode : STD_LOGIC_VECTOR(1 DOWNTO 0);
+BEGIN
 
-MainDecoder : PROCESS(OP, Regwrite, RegDst, ALUSrc, Branch, MemRead, MemWrite, MemtoReg, ALUOp, Jump) 
-	BEGIN
-	
-	case OP is 
-	when "000000"   -- R-Type
-		RegWrite => 1; 
-		RegDst => 1;
-		ALUSrc => 0;
-		Branch => 0;
-		MemRead => 0;
-		MemWrite => 0;
-		MemtoReg => 0;
-		ALUOp => B"10";
-		Jump => "0"
+MainDecoder : PROCESS(OP, MtoReg, MemW, MemR, MRead, Bran, ALUS, RegD, RegW, J, ALUCode) 
+
+BEGIN
+	case (OP) is 
+	when "000000" =>  -- R-Type
+		RegW <= '1'; 
+		RegD <= '1';
+		ALUS <= '0';
+		Bran <= '0';
+		MemR <= '0';
+		MemW <= '0';
+		MtoReg <= '0';
+		ALUCode <= "10";
+		J <= '0';
 	--	HexCode => X"304"
 	
-	when "100011"	--Lw
-		RegWrite => 1; 
-		RegDst => 0;
-		ALUSrc => 1;
-		Branch => 0;
-		MemRead => 1;
-		MemWrite => 0;
-		MemtoReg => 1;
-		ALUOp => B"00";
-		Jump => "0"
+	when "100011" => --Lw
+		RegW <= '1'; 
+		RegD <= '0';
+		ALUS <= '1';
+		Bran <= '0';
+		MemR <= '1';
+		MemW <= '0';
+		MtoReg <= '1';
+		ALUCode <= "00";
+		J <= '0';
 	--	HexCode => X"2A8"
 	
-	when "101011"	--Sw
-		RegWrite => 0; 
-		RegDst => '-';
-		ALUSrc => 1;
-		Branch => 0;
-		MemRead => 0;
-		MemWrite => 1;
-		MemtoReg => '-';
-		ALUOp => B"00";
-		Jump => "0"
+	when "101011" => --Sw
+		RegW <= '0'; 
+		RegD <= '-';
+		ALUS <= '1';
+		Bran <= '0';
+		MemR <= '0';
+		MemW <= '1';
+		MtoReg <= '-';
+		ALUCode <= "00";
+		J <= '0';
 	--	HexCode => X"090"
 
-	when "000100"	--Beq
-		RegWrite => 0; 
-		RegDst => '-';
-		ALUSrc => 0;
-		Branch => 1;
-		MemRead => 0;
-		MemWrite => 0;
-		MemtoReg => '-';
-		ALUOp => B"01";
-		Jump => "0"
+	when "000100" => --Beq
+		RegW <= '0'; 
+		RegD <= '-';
+		ALUS <= '0';
+		Bran <= '1';
+		MemR <= '0';
+		MemW <= '0';
+		MtoReg <= '-';
+		ALUCode <= "01";
+		J <= '0';
 	--	HexCode => X"042"
 
-	when "001000"	--Addi
-		RegWrite => 1; 
-		RegDst => 0;
-		ALUSrc => 1;
-		Branch => 0;
-		MemRead => 0;
-		MemWrite => 0;
-		MemtoReg => 0;
-		ALUOp => B"00";
-		Jump => "0"
+	when "001000" => --Addi
+		RegW <= '1'; 
+		RegD <= '0';
+		ALUS <= '1';
+		Bran <= '0';
+		MemR <= '0';
+		MemW <= '0';
+		MtoReg <= '0';
+		ALUCode <= "00";
+		J <= '0';
 	--	HexCode => X"280"
 
-	when "000010"	--J
-		RegWrite => 0; 
-		RegDst => '-';
-		ALUSrc => '-';
-		Branch => '-';
-		MemRead => 0;
-		MemWrite => 0;
-		MemtoReg => '-';
-		ALUOp => B"'--'";
-		Jump => "0"
+	when others => --J
+		RegW <= '0'; 
+		RegD <= '-';
+		ALUS <= '-';
+		Bran <= '-';
+		MemR <= '0';
+		MemW <= '0';
+		MtoReg <= '-';
+		ALUCode <= "00";
+		J <= '0';
 	--	HexCode => X"001"
 
-	end case
-END PROCESS
+	end case;
+END PROCESS;
 
 ALUDecoder : PROCESS
+BEGIN
 
-END PROCESS 
+END PROCESS;
+
+END logique;
