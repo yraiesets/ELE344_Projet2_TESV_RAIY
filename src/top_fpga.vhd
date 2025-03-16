39d4d6e3643f44703c715fabc43a88aa8082eac9
yraies <yasser.raies.1@ens.etsmtl.ca>
2025-03-16 19:28:36 -0400

    derniere maj

--- src/top_fpga.vhd
+++ src/top_fpga.vhd
@@ -1,12 +1,12 @@
 --========================= top_fpga.vhd ============================
--- ELE-343 Conception des syst�mes ordin�s
--- HIVER 2017, Ecole de technologie sup�rieure
--- Auteur : Yves Blaqui�re
+-- ELE-343 Conception des systemes ordines
+-- HIVER 2017, Ecole de technologie superieure
+-- Auteur : Yves Blaquiere
 -- =============================================================
 -- Description: top_fpga
 --              Enveloppe (wrapper) pour le top du MIPS qui
 --              Nomme les ports en fonction du fichier des pins
---              du FPGA, tel que d�crit dans le fichier
+--              du FPGA, tel que decrit dans le fichier
 --              DE2_pin_assignments.csv du DE2
 --              Ajoute des afficheurs 7-segment sur les ports de
 --              sortie du top
@@ -18,30 +18,34 @@ USE ieee.std_logic_1164.ALL;
 ENTITY TOP_FPGA IS
 
 	PORT(
-		KEY  : IN  STD_LOGIC_VECTOR(0 TO 1);	-- KEY[0]=reset, KEY[1]=clock
-		HEX0 : OUT STD_LOGIC_VECTOR(0 TO 6);	-- PC(3 downto 0) 
-		HEX1 : OUT STD_LOGIC_VECTOR(0 TO 6);	-- PC(7 downto 4) 
-		HEX2 : OUT STD_LOGIC_VECTOR(0 TO 6);	-- DataAddress(3 downto 0) 
-		HEX3 : OUT STD_LOGIC_VECTOR(0 TO 6);	-- DataAddress(7 downto 4) 
-		HEX4 : OUT STD_LOGIC_VECTOR(0 TO 6);	-- WriteData(3 downto 0) 
-		HEX5 : OUT STD_LOGIC_VECTOR(0 TO 6)	-- WriteData(7 downto 4
+		KEY  : IN  STD_LOGIC_VECTOR(0 TO 1);  -- KEY[0]=reset, KEY[1]=clock
+		HEX0 : OUT STD_LOGIC_VECTOR(0 TO 6);  -- PC(3 downto 0) 
+		HEX1 : OUT STD_LOGIC_VECTOR(0 TO 6);  -- PC(7 downto 4) 
+		HEX2 : OUT STD_LOGIC_VECTOR(0 TO 6);  -- DataAddress(3 downto 0) 
+		HEX3 : OUT STD_LOGIC_VECTOR(0 TO 6);  -- DataAddress(7 downto 4) 
+		HEX4 : OUT STD_LOGIC_VECTOR(0 TO 6);  -- WriteData(3 downto 0) 
+		HEX5 : OUT STD_LOGIC_VECTOR(0 TO 6)   -- WriteData(7 downto 4)
 	);
 
 END ENTITY TOP_FPGA;
 
 ARCHITECTURE rtl OF top_fpga IS
-	SIGNAL Memwrite               : STD_LOGIC;
-	SIGNAL PC                     : STD_LOGIC_VECTOR(31 DOWNTO 0);
-	SIGNAL WriteData, DataAddress : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL Memwrite                : STD_LOGIC;
+	SIGNAL PC                      : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL WriteData, DataAddress  : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL ResetIntern             : STD_LOGIC;    
 BEGIN
+
+	ResetIntern <= NOT(KEY(0)); -- Necessaire, car evaluation dynamique (compile-time)
+
 	-- Instantiation du top
 	DUT : ENTITY work.TOP(rtl)
 		PORT MAP(
-			Clk		=> KEY(1),
-			Reset		=> NOT(KEY(0)),
-			PC		=> PC,
-			WriteData	=> WriteData,
-			DataAddress	=> DataAddress
+			Clk        => KEY(1),
+			Reset      => ResetIntern,
+			PC         => PC,
+			WriteData  => WriteData,
+			DataAddress => DataAddress
 		);
 
 	-- Afficheurs 7-segments pour les ports de sortie
